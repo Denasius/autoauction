2aa6062096aae2b3ec899006d77cedc18027a074
Denasius <den_kostyuk1989@mail.ru>
2019-06-03 17:09:46 +0300

    Тестовый коммент

--- app/Http/Controllers/Admin/CategoriesController.php
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -16,6 +16,7 @@ class CategoriesController extends Controller
     public function index()
     {
         $categories = Category::all();
+       
         return view('admin.categories.index', ['categories' => $categories]);
     }
 
