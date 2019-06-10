35d8acb4692cce277606e8883969e21d7a51b592
Psiu <psiu7217@gmail.com>
2019-06-10 17:57:41 +0300

    Исправил ошибки мерджа

--- app/Attribute.php
+++ app/Attribute.php
@@ -10,18 +10,11 @@ use Cviebrock\EloquentSluggable\Sluggable;
 class Attribute extends Model
 {
     use Sluggable;
-<<<<<<< HEAD
-=======
     protected $fillable = ['title', 'type_id'];
->>>>>>> 5126534dc5ef60070b04fff219d2762e6a58680f
 
     //Получаем все атрибутоы по типу
     public function get_attribute_by_type($type) {
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 5126534dc5ef60070b04fff219d2762e6a58680f
         if (intval($type)) {
             $results = Attribute::where('type_id', $type)->get();
         }else {
@@ -30,33 +23,19 @@ class Attribute extends Model
             $results = Attribute::where('type_id', $type['id'])->get();
         }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 5126534dc5ef60070b04fff219d2762e6a58680f
         return $results;
     }
 
     public function add($fields) {
         $Attribute = new Attribute();
-<<<<<<< HEAD
-        $Attribute->title = $fields['title'];
-        $Attribute->type_id = $fields['type_id'];
-=======
         $Attribute->fill($fields);
->>>>>>> 5126534dc5ef60070b04fff219d2762e6a58680f
         $Attribute->save();
         return true;
     }
 
     public function edit($fields){
         $AttributeType = Attribute::find($fields['id']);
-<<<<<<< HEAD
-        $AttributeType->title = $fields['title'];
-        $AttributeType->type_id = $fields['type_id'];
-=======
         $AttributeType->fill($fields);
->>>>>>> 5126534dc5ef60070b04fff219d2762e6a58680f
         $AttributeType->save();
     }
 
