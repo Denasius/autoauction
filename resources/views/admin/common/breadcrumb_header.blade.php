8bf0813a690742e7c3261bd5fcfe8b1d520b61c8
Psiu <psiu7217@gmail.com>
2019-06-14 18:04:02 +0300

    Хз что это

--- resources/views/admin/common/breadcrumb_header.blade.php
+++ resources/views/admin/common/breadcrumb_header.blade.php
@@ -12,31 +12,34 @@
         </ol>
     </div>
 </div>
-
-
 <div class="row">
     <div class="col-sm-12">
         <div class="header_block">
             <h1 class="page-header">{!! $breadcrumb_header['icon'] !!}{!! $breadcrumb_header['title'] !!}</h1>
             @if(isset($filter))
                 <div class="col-sm-3">
-
-                    <select id="filter_id" name="filter_id" class="form-control">
-                        <option value="{{URL::current()}}">Нет</option>
-                        @foreach($filter as $item)
-                            <option value="{{URL::current()}}?filter_id={{$item->id}}"
-                            @if (isset($filter_id) && $filter_id == $item->id) selected @endif
-                            >{{$item->title}}</option>
-                        @endforeach
-                    </select>
+                    <div class="d_flex">
+                        @isset($filter_name)
+                            <span style="margin-right: 15px">{{$filter_name}}</span>
+                        @endisset
+                        <select id="filter_id" name="filter_id" class="form-control">
+                            <option value="{{URL::current()}}">Нет</option>
+                            @foreach($filter as $item)
+                                <option value="{{URL::current()}}?filter_id={{$item->id}}"
+                                        @if (isset($filter_id) && $filter_id == $item->id) selected @endif
+                                >{{$item->title}}</option>
+                            @endforeach
+                        </select>
+                    </div>
                 </div>
                 <script>
                     $('#filter_id').change(function () {
-                        $(location).attr('href',$(this).val());
+                        $(location).attr('href', $(this).val());
                     });
                 </script>
             @endif
-            <a class="btn {{$breadcrumb_header['btn']['class']}}" href="{{route($breadcrumb_header['btn_href'])}}" title="{{$breadcrumb_header['btn']['text']}}">{{$breadcrumb_header['btn']['text']}}</a>
+            <a class="btn {{$breadcrumb_header['btn']['class']}}" href="{{route($breadcrumb_header['btn_href'])}}"
+               title="{{$breadcrumb_header['btn']['text']}}">{{$breadcrumb_header['btn']['text']}}</a>
         </div>
     </div>
 </div>
